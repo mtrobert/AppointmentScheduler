8bcd9d71ef1e6dc1e07535eca2d06299b7bb1808
Robert Marczak <robert.m.1305@gmail.com>
2021-05-25 15:44:12 +0100

    Add Sign Up View
    Added sign up view with email, password and remember me  fields. Also There is a link to register at the bottom of the page for users to sign up.

--- AppointmentScheduler/Controllers/AccountController.cs
+++ AppointmentScheduler/Controllers/AccountController.cs
@@ -16,7 +16,7 @@ namespace AppointmentScheduler.Controllers
             _db = db;
         }
 
-        public IActionResult Index()
+        public IActionResult Login()
         {
             return View();
         }
--- AppointmentScheduler/Startup.cs
+++ AppointmentScheduler/Startup.cs
@@ -25,8 +25,8 @@ namespace AppointmentScheduler
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllersWithViews();
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
