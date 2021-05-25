9c45bf33c536814c01a4e2f6130714560f74321b
Robert Marczak <robert.m.1305@gmail.com>
2021-05-25 22:47:25 +0100

    Add Register ViewModel
    Added register view model as well as the class with the role types so it can be used as the dropdown when registering.

---
+++ AppointmentScheduler/Helper/Helper.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppointmentScheduler.Helper
+{
+    public static class Helper
+    {
+        private static string Admin = "Admin";
+        private static string Client = "Client";
+        private static string Hairdresser = "Hairdresser";
+
+        public static List<SelectListItem> DropDown_GetRoles()
+        {
+            return new List<SelectListItem>
+            {
+                new SelectListItem{Value = Helper.Admin, Text = Helper.Admin},
+                new SelectListItem{Value = Helper.Hairdresser, Text = Helper.Hairdresser},
+                new SelectListItem{Value = Helper.Client, Text = Helper.Client}
+            };
+        }
+    }
+}
---
+++ AppointmentScheduler/Models/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppointmentScheduler.Models.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 8)]    
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Compare("Password", ErrorMessage ="The passwords do not match. Please try again.")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        [Display(Name = "Role Type")]
+        public string RoleType { get; set; }
+    }
+}
