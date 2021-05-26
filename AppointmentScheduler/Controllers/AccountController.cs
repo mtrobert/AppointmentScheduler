448b04d1e1f220d8e204ad1bd87dcd0e6c11936d
Robert Marczak <robert.m.1305@gmail.com>
2021-05-26 10:27:02 +0100

    Add RegisterPost
    Added register mechanism to the register post action. After the registration is successful the user is loged in automatically.

--- AppointmentScheduler/Controllers/AccountController.cs
+++ AppointmentScheduler/Controllers/AccountController.cs
@@ -1,4 +1,6 @@
 ﻿using AppointmentScheduler.Models;
+using AppointmentScheduler.Models.ViewModels;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -10,10 +12,16 @@ namespace AppointmentScheduler.Controllers
     public class AccountController : Controller
     {
         private readonly ApplicationDbContext _db;
+        private UserManager<ApplicationUser> _userManager;
+        private SignInManager<ApplicationUser> _signInManager;
+        private RoleManager<IdentityRole> _roleManager;
 
-        public AccountController(ApplicationDbContext db)
+        public AccountController(ApplicationDbContext db, UserManager<ApplicationUser> um, SignInManager<ApplicationUser> sm, RoleManager<IdentityRole> rm)
         {
             _db = db;
+            _userManager = um;
+            _signInManager = sm;
+            _roleManager = rm;
         }
 
         public IActionResult Login()
@@ -25,5 +33,29 @@ namespace AppointmentScheduler.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser
+                {
+                    Name = model.Name,
+                    Email = model.Email,
+                    UserName = model.Email
+                };
+
+                var result = await _userManager.CreateAsync(user);
+                if(result.Succeeded)
+                {
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            return View();
+        }
     }
+
 }
