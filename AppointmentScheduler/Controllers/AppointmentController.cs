0ff49adcbb78e4776ee5dd7fc0fb64dcfdcec5dd
Robert Marczak <robert.m.1305@gmail.com>
2021-05-27 23:34:43 +0100

    Display The Clients And The Hairdressers
    Added VMs for clients and hairdresser and the list containing either of them is passed to the view using viewbags.

--- AppointmentScheduler/Controllers/AppointmentController.cs
+++ AppointmentScheduler/Controllers/AppointmentController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AppointmentScheduler.Services;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,8 +9,16 @@ namespace AppointmentScheduler.Controllers
 {
     public class AppointmentController : Controller
     {
+        private readonly IAppointmentService _appointmentService;
+
+        public AppointmentController(IAppointmentService appointmentService)
+        {
+            _appointmentService = appointmentService;
+        }
+
         public IActionResult Index()
         {
+            ViewBag.HairdresserList = _appointmentService.GetClientList();
             return View();
         }
     }
