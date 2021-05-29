4c4df1a0d985278a692361de06d705992dfc35d7
Robert Marczak <robert.m.1305@gmail.com>
2021-05-29 23:44:31 +0100

    Add API End Point
    Added API end point for the appointment, so it can be used to updare the datebase when creating, editing or deleting  the appointment.

---
+++ AppointmentScheduler/Controllers/API/API_appointmentController.cs
@@ -0,0 +1,34 @@
+﻿using AppointmentScheduler.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace AppointmentScheduler.Controllers.API
+{
+    [Route("api/Appointment")]
+    [ApiController]
+    public class API_appointmentController : Controller
+    {
+        private readonly IAppointmentService _aS;
+        private readonly IHttpContextAccessor _httpCA;
+        private readonly string _loginUserId;
+        private readonly string _role;
+
+        public API_appointmentController(IAppointmentService aS, IHttpContextAccessor httpCA)
+        {
+            _aS = aS;
+            _httpCA = httpCA;
+            _loginUserId = _httpCA.HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier);
+            _role = _httpCA.HttpContext.User.FindFirstValue(ClaimTypes.Role);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
