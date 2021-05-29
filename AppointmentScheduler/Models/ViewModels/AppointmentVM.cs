a9fc79f6dd462c410b622c28d46705578c96f617
Robert Marczak <robert.m.1305@gmail.com>
2021-05-29 23:46:32 +0100

    Change The View
    Changed the view to make sure it has the buttons to interact with the appointment.

---
+++ AppointmentScheduler/Models/ViewModels/AppointmentVM.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppointmentScheduler.Models.ViewModels
+{
+    public class AppointmentVM
+    {       
+        public int? Id { get; set; }
+        public string Title { get; set; }
+        public string Desctiption { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int Duration { get; set; }
+        public string HairdresserId { get; set; }
+        public string ClientId { get; set; }
+        public string AdminId { get; set; }
+        public bool IsDoctorApproved { get; set; }
+
+        public string HairdresserName { get; set; }
+        public string ClientName{ get; set; }
+        public string AdminName { get; set; }
+        public bool IsForClient { get; set; }
+    }
+}
